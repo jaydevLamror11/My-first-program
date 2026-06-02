f407b9e34066bb20aa97d8b1d7e88ea234eab48e
jaydevlamror019-lab <jaydevlamror019@gmail.com>
2026-06-02 10:43:09 +0000

    added whitespace and capitalize the user's name.

--- helo.py
+++ helo.py
@@ -1,4 +1,8 @@
-name=input("what is your name ?")
-print(f"hello {name}""how are you")
+#ask user his/her name,remove whitespace and capitalize the name of the user
+name=input("what is your name ?") . strip() . title()
+
+
+#say hello to the user
+print(f"hello {name}"",how are you")
 
 
