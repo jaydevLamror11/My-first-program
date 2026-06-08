ef0a3bf74902c8ba908ef93df8ab87ebf0b6d5fb
jaydevLamror11 <jaydevlamror019@gmail.com>
2026-06-08 07:40:59 +0530

    minor changes

--- calculator.py
+++ calculator.py
@@ -5,7 +5,7 @@ def main():
 
 
 def square(n):
-    return n*n
+    return n**2
 
 main()
 
--- helo.py
+++ helo.py
@@ -1,8 +1,8 @@
 #ask user his/her name,remove whitespace and capitalize the name of the user
-name=input("what is your name ?") . strip() . title()
+name=input("what is your name?") . strip() . title()
 
 
 #say hello to the user
-print(f"hello {name}"",how are you")
+print(f"hello {name}"",how are you?")
 
 
