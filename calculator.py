14935764dd365e49ce772cab524203dc35e44b73
jaydevLamror11 <jaydevlamror019@gmail.com>
2026-06-09 14:05:28 +0530

    int to float

--- calculator.py
+++ calculator.py
@@ -1,19 +1,13 @@
 def main():
-    x = int(input("what's x? "))
-    print("x cube is " , cube (x))
+    x = float(input("what's x? "))
+    print("x cube is " , cube(x))
 
 
 
-def  cube(n):
+def cube(n):
     return n**3
 
 main()
-
-
-
-
-
-
 
 
 
