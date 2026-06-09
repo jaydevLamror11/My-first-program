9b598c82aa0701b3e2e6fc0545145f2a69f50840
jaydevLamror11 <jaydevlamror019@gmail.com>
2026-06-09 15:00:04 +0530

    using define function

--- hello.py
+++ hello.py
@@ -1,4 +1,10 @@
-#ask user his/her name
-name=input("what's your name?")
-#say hello to the user and ask how are you
-print(f"hello {name}"",how are you")
+def hello(to="world"):
+    print ("hello," ,to)
+    
+    
+    
+    
+hello()
+name = input("what's your name? ")
+hello(name)
+
