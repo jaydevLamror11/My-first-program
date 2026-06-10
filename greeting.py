d31c5677f3cd41d381591bcbd9794d469fdedeb5
jaydevLamror11 <jaydevlamror019@gmail.com>
2026-06-10 21:41:04 +0530

    greetings everybody

---
+++ greeting.py
@@ -0,0 +1,10 @@
+def greet(input):
+    if "hello" in input:
+        return "hello, there"
+    else:
+        return " I'am not sure what do you mean?"
+    
+
+
+greeting = greet("hello, computer")
+print(" yoo," + greeting)
