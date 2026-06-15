5350cc957d9e0318d562a731fe95eb7fb225de0e
jaydevLamror11 <jaydevlamror019@gmail.com>
2026-06-15 21:10:16 +0530

    added else

--- faces/faces.py
+++ faces/faces.py
@@ -1,15 +1,17 @@
 
-def emogi():
+def emoji():
     face = input("heey: ")
     return face
 
 def main():
-    face = emogi()
+    face = emoji()
     if face == "Hello :)":
         print("Hello 🙂")
     if face == "Goodbye :(":
         print("Goodbye 🙁")
     if face == ("Hello :) Goodbye :("):
         print("Hello 🙂 Goodbye 🙁")
+    else:
+        print("I don't recognize that emoji text. ")
 main()
 
