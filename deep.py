b994de47d721f89701466d37724defe28bb5e62e
jaydevLamror11 <jaydevlamror019@gmail.com>
2026-06-17 07:58:39 +0530

    match

--- deep.py
+++ deep.py
@@ -2,7 +2,7 @@
 uni = input("What is the answer to the great question of life , the Universe, and everythin? ")
 
 match uni:
-    case "42":
+    case "42" | "Forty Two" | "forty_two":
         print ("yes! 🙂")
     case _:
         print("noo 🙁")
