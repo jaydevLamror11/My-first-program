86d62449fad4803bebc5260b1b077a6d0d5283cf
jaydevlamror019-lab <jaydevlamror019@gmail.com>
2026-06-22 01:43:44 +0000

    CS50P P SET 1 SOL-V

---
+++ meal/meal.py
@@ -0,0 +1,14 @@
+def main ():
+    time = convert(input("What time is it? "))
+    return time
+
+
+
+
+def convert(time):
+    hours, minutes = time.split(":")
+
+
+if __name__ == "__main__":
+    main()
+
