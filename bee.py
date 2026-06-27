e8f3bfe85bd861e1b1677d9cd6c654664e356313
jaydevLamror11 <jaydevlamror019@gmail.com>
2026-06-27 22:00:07 +0530

    Document lessons learned about dictionaries and pop()

--- bee.py
+++ bee.py
@@ -1,4 +1,4 @@
-WORDS = {"HAIR": 4, "CHAIR": 4, "PAIR": 4}
+WORDS = {"HAIR": 4, "PAIR": 4, "CHAIR": 5, "GRAPHIC": 7}
 
 def main():
     print("Welcome to spelling Bee!")
@@ -6,10 +6,21 @@ def main():
 
     while len(WORDS) > 0:
         print(f"{len(WORDS)} words left!")
-        guess = input("Guess a word:")
+        guess = input("Guess a word: "). upper()
+    
 
 
         # TODO: check if guess in dictionaty
+        if guess == "GRAPHIC":
+            WORDS.clear()
+            print("You have won the Game! ")
+        elif guess in WORDS.keys():
+            points = WORDS.pop(guess)
+            print(f"Good job! You scored {points} points.")
+        else:
+            print("oops! Try again")
+    for word, points in WORDS.items():
+        print(f"{word} was worth {points} points.")
 
     print("That's the game!")
 
