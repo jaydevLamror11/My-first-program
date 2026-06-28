1ded654f0d2c752d21daeacf8de5a2766172419f
jaydevLamror11 <jaydevlamror019@gmail.com>
2026-06-28 07:15:51 +0530

    debug the written code

--- letters.py
+++ letters.py
@@ -1,7 +1,7 @@
 def main():
     names = ["Mario", "Luigi", "Daisy", "Yoshi"]
-    for i in range(len(names)):
-                   print(write_letter(names[i], "Princess Peach"))
+    for name in names:
+         print(write_letter(name, "Princess Peach"))
 
 def write_letter(reciever, sender):
     return f"""
